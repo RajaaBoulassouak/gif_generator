436838e749574a0a2c9f77d0f1d8c6e589edd204
Seth Schwartz <sschwartz214@gmail.com>
2018-07-19 07:53:03 -0600

    adds navbar to user show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-# category_1 = Category.create(name: 'funny')
-# gif_1 = category_1.gifs.create(image_url: "https://media2.giphy.com/media/JIX9t2j0ZTN9S/200w.webp")
-#
-# category_2 = Category.create(name: 'happy')
-# gif_2 = category_2.gifs.create(image_url: "https://media1.giphy.com/media/BlVnrxJgTGsUw/giphy.webp")
-#
-# category_3 = Category.create(name: 'dance')
-# gif_3 = category_3.gifs.create(image_url: "https://media3.giphy.com/media/4Zo1InomnFJWIwIkf7/200w.webp")
-#
-# category_4 = Category.create(name: 'basketball')
-# gif_4 = category_4.gifs.create(image_url: "https://media3.giphy.com/media/PIIMXAjqlO9zy/200w.webp")
-#
-# category_5 = Category.create(name: 'dog')
-# gif_5 = category_5.gifs.create(image_url: "https://media1.giphy.com/media/l46Cu9f4i5W6E6DjW/200w.webp")
+category_1 = Category.create(name: 'funny')
+gif_1 = category_1.gifs.create(image_url: "https://media2.giphy.com/media/JIX9t2j0ZTN9S/200w.webp")
+
+category_2 = Category.create(name: 'happy')
+gif_2 = category_2.gifs.create(image_url: "https://media1.giphy.com/media/BlVnrxJgTGsUw/giphy.webp")
+
+category_3 = Category.create(name: 'dance')
+gif_3 = category_3.gifs.create(image_url: "https://media3.giphy.com/media/4Zo1InomnFJWIwIkf7/200w.webp")
+
+category_4 = Category.create(name: 'basketball')
+gif_4 = category_4.gifs.create(image_url: "https://media3.giphy.com/media/PIIMXAjqlO9zy/200w.webp")
+
+category_5 = Category.create(name: 'dog')
+gif_5 = category_5.gifs.create(image_url: "https://media1.giphy.com/media/l46Cu9f4i5W6E6DjW/200w.webp")
